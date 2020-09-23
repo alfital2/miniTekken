a9dacd5d5d18e582554bf40fa11f3b1cf821fe20
tal <alfital2@gmail.com>
2020-09-24 01:02:19 +0300

    pushing new files - dynamicUpdates - for the css updating process

---
+++ assets/scripts/dynamicUpdate.js
@@ -0,0 +1,24 @@
+let blockedButtons = `dynamicElement {
+    background: grey;
+    border: 1px solid grey;
+    disable: true;
+    }
+    dynamicElement:hover,
+    dynamicElement:active {
+    background: grey;
+    border-color: grey;
+    }`;
+
+let unBlockesButtons = `dynamicElement {
+    background: #a2112a;
+    border: 1px solid #a2112a;
+    disable: false;
+    }
+    dynamicElement:hover,
+    dynamicElement:active {
+    background: #f37a64;
+    border-color: #f37a64;
+    }`;
+
+
+    f37a64
